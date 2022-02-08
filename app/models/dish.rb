26e301ccf8ffea020094f34a0371f9cbd1e35db0
Rey David <redacuve@gmail.com>
2022-02-08 01:43:39 -0600

    create validator for the price of the dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,14 @@
+class PriceValidator < ActiveModel::Validator
+  def validate(record)
+    if record.price && record.menu_id && (record.menu.prices + record.price) == 77
+      record.errors.add :base, "The sum of the prices of the dishes are exactly 77"
+    end
+  end
+end
+
 class Dish < ApplicationRecord
   belongs_to :menu
   validates :name, presence: true, format: { with: /\A[^E]+\w+\z/i }, uniqueness: true
   validates :price, presence: true
+  validates_with PriceValidator, fields: [:price]
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
   has_many :dish
   validates :name, presence: true
+  
+  def prices
+    dish.sum(:price)
+  end
 end
